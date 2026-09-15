38903e4ca3a91ed066878b4f4b82070e46a772e3
Mitchell Hanberg <mitch@mitchellhanberg.com>
2026-09-15 09:20:14 -0400

    chore: prepare for release

--- mix.exs
+++ mix.exs
@@ -1,13 +1,18 @@
 defmodule TableauDevServer.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/elixir-tools/tableau_dev_server"
   def project do
     [
       app: :tableau_dev_server,
       version: "0.0.1",
+      description: "Development server for Tableau",
+      source_url: @source_url,
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      docs: docs()
     ]
   end
 
@@ -27,7 +32,29 @@ defmodule TableauDevServer.MixProject do
       {:websock_adapter, "~> 0.5"},
 
       # dev
-      {:styler, "~> 1.0", only: :dev}
+      {:styler, "~> 1.0", only: :dev},
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Mitchell Hanberg"],
+      licenses: ["MIT"],
+      links: %{
+        GitHub: @source_url,
+        Sponsor: "https://github.com/sponsors/mhanberg"
+      },
+      files: ~w(lib LICENSE mix.exs README.md .formatter.exs)
+    ]
+  end
+
+  defp docs do
+    [
+      main: "TableauDevServer",
+      extras: [
+        "README.md"
+      ]
     ]
   end
 end
