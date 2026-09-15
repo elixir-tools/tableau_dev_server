4fdd194645a8806bb1c88dff8afba0c7c11b04d5
Mitchell Hanberg <mitch@mitchellhanberg.com>
2026-09-15 09:08:19 -0400

    chore: reduce elixir constraint

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TableauDevServer.MixProject do
     [
       app: :tableau_dev_server,
       version: "0.0.1",
-      elixir: "~> 1.18",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
